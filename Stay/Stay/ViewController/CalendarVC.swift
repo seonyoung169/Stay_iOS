00127622ce44cff10b923a8ee7da57c97993058f
sunnyangee <hwagae2003@gmail.com>
2021-01-12 12:02:11 +0900

    [Calendar] navigation back btn 적용

--- Stay/Stay/ViewController/CalendarVC.swift
+++ Stay/Stay/ViewController/CalendarVC.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class CalendarVC: UIViewController {
 
+    @IBOutlet weak var backBtn: UIImageView!
+    @IBOutlet weak var yearAndMonth: NSLayoutConstraint!
+    @IBOutlet weak var currentCount: UILabel!
+    @IBOutlet weak var bestCount: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setBackBtnTapGesture()
     }
     
-
+    func setBackBtnTapGesture() -> Void {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tap))
+        self.backBtn.addGestureRecognizer(gesture)
+        self.backBtn.isUserInteractionEnabled = true
+    }
+    
+    @objc func tap(sender : UIGestureRecognizer){
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
