a037bff2c7a12059126ffe8a8794da7bae4afe5f
sunnyangee <hwagae2003@gmail.com>
2021-01-20 13:15:43 +0900

    [Calendar] calenter 하단 회색 라인, 스탬프 이미지 추가

--- Stay/Stay/ViewController/CalendarVC.swift
+++ Stay/Stay/ViewController/CalendarVC.swift
@@ -9,7 +9,7 @@ import UIKit
 import FSCalendar
 
 class CalendarVC: UIViewController {
-
+    
     @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var backBtn: UIImageView!
     @IBOutlet weak var yearAndMonth: NSLayoutConstraint!
@@ -43,25 +43,54 @@ extension CalendarVC : FSCalendarDelegate, FSCalendarDataSource {
         self.calendar.locale = Locale(identifier: "ko_KR")
         self.calendar.appearance.weekdayTextColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
         self.calendar.allowsSelection = false
+        
         self.calendar.register(CalendarCell.self, forCellReuseIdentifier: "CalendarCell")
     }
     
     func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
+        
         let cell = calendar.dequeueReusableCell(withIdentifier: "CalendarCell", for: date, at: position) as! CalendarCell
+        
+        let monthFormatter = DateFormatter()
+        monthFormatter.dateFormat = "yyyy-MM"
         
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         
-        if(formatter.string(from : date) < formatter.string(from : Date())){
-            cell.backgroundView = UIImageView(image: UIImage(named: "icStamp"))
-            
-            let len = Int ((calendar.frame.size.width / 7) * 0.66)
-            cell.backgroundView?.frame = CGRect(x: 0, y: 0, width: len, height: len)
+        let callLen = Int (calendar.frame.width / 7)
+        let stampLen = Int(callLen * 2 / 3)
         
-            cell.backgroundView?.translatesAutoresizingMaskIntoConstraints = false
-            cell.backgroundView?.centerXAnchor.constraint(equalTo: cell.centerXAnchor).isActive = true
-            cell.backgroundView?.centerYAnchor.constraint(equalTo: cell.centerYAnchor).isActive = true
+        let basicView = UIView(frame: CGRect(x: 0, y: 0, width: callLen, height: callLen))
+        
+        
+        //this month
+        if(monthFormatter.string(from: date) == monthFormatter.string(from: Date())){
+            cell.backgroundView?.frame = CGRect(x: 0, y: 0, width: callLen, height: callLen)
+        
+            let line = UIView(frame: CGRect(x: 0, y: callLen-1, width: callLen, height: 1))
+            
+            line.backgroundColor = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1 )
+            basicView.addSubview(line)
+            
+            line.translatesAutoresizingMaskIntoConstraints = false
+            line.leadingAnchor.constraint(equalTo: basicView.leadingAnchor).isActive = true
+            line.trailingAnchor.constraint(equalTo: basicView.trailingAnchor).isActive = true
+            line.bottomAnchor.constraint(equalTo: basicView.bottomAnchor).isActive = true
+            line.heightAnchor.constraint(equalToConstant: 1).isActive = true
+            
+            if(formatter.string(from : date) < formatter.string(from : Date())){ // set stamp image
+                let stamp = UIImageView(image: UIImage(named: "icStamp"))
+                stamp.frame = CGRect(x: 0, y: 0, width: stampLen, height: stampLen)
+                basicView.addSubview(stamp)
+                
+                stamp.translatesAutoresizingMaskIntoConstraints = false
+                stamp.centerXAnchor.constraint(equalTo: basicView.centerXAnchor).isActive = true
+                stamp.centerYAnchor.constraint(equalTo: basicView.centerYAnchor).isActive = true
+            }
+        }else{  // not this month
         }
+        
+        cell.backgroundView = basicView
         
         return cell
     }
