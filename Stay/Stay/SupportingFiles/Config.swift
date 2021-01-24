df0f079d9b437a50f39ecb02870f629cd0e4effd
sunnyangee <hwagae2003@gmail.com>
2021-01-24 20:13:17 +0900

    [Friend] 통신 적용 완료

---
+++ Stay/Stay/SupportingFiles/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  Stay
+//
+//  Created by SEONYOUNG LEE on 2021/01/20.
+//
+
+import Foundation
+
+struct Config {
+    static let baseURL = "http://ec2-3-36-132-49.ap-northeast-2.compute.amazonaws.com:8080"
+}
