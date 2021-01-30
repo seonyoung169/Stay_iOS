7f338362ac5c9e988f14385a26704539b585a8c1
sunnyangee <hwagae2003@gmail.com>
2021-01-30 14:11:34 +0900

    [Setting] 내 정보 수정 화면 드롭다운 제외 완성

--- Stay/Stay/ViewController/EditMyInfoVC.swift
+++ Stay/Stay/ViewController/EditMyInfoVC.swift
@@ -8,22 +8,53 @@
 import UIKit
 
 class EditMyInfoVC: UIViewController {
-
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var homeAddressArea: UIView!
+    @IBOutlet weak var homeAddressLabel: UILabel!
+    @IBOutlet weak var workAddressArea: UIView!
+    @IBOutlet weak var workAddressLabel: UILabel!
+    @IBOutlet weak var theOtherClassArea: UIView!
+    @IBOutlet weak var theOtherAdressArea: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setImageView()
+        setBackgroundBox()
+        setTapGesture()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setImageView() {
+        profileImageView.layer.cornerRadius = 24
     }
-    */
+    
+    func setBackgroundBox() {
+        homeAddressArea.layer.cornerRadius = 8
+        homeAddressArea.layer.borderWidth = 0.5
+        homeAddressArea.layer.borderColor = CGColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1)
+        
+        workAddressArea.layer.cornerRadius = 8
+        workAddressArea.layer.borderWidth = 0.5
+        workAddressArea.layer.borderColor = CGColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1)
+        
+        theOtherClassArea.layer.cornerRadius = 8
+        theOtherClassArea.layer.borderWidth = 0.5
+        theOtherClassArea.layer.borderColor = CGColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1)
+        
+        theOtherAdressArea.layer.cornerRadius = 8
+        theOtherAdressArea.layer.borderWidth = 0.5
+        theOtherAdressArea.layer.borderColor = CGColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1)
+    }
+    
+    func setTapGesture() {
+        let addressTapGesture = UITapGestureRecognizer(target: self, action: #selector(tapAddress(sender:)))
+        
+        self.theOtherAdressArea.addGestureRecognizer(addressTapGesture)
+    }
+    
+    @objc func tapAddress(sender : UIGestureRecognizer){
+        print("address tap")
+    }
 
 }
